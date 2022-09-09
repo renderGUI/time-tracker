cfdba3e340dc088f244f8bdfbd3f4031105cf927
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-09-09 13:43:11 -0400

    Implement stopwatch functionality

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,9 +1,31 @@
 import classes from "./Stopwatch.module.scss";
+import { useState, useEffect } from "react";
 
-const Stopwatch = () => {
+const Stopwatch = (props) => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    let stopwatch;
+    if (props.stopwatchIsRunning) {
+      stopwatch = setInterval(() => {
+        setSeconds((prevState) => prevState + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(stopwatch);
+    };
+  }, [props.stopwatchIsRunning]);
+
+  const convertToHHMMSS = (seconds) => {
+    const d = new Date(null);
+    d.setSeconds(seconds);
+    return d.toISOString().substr(11, 8);
+  };
+
   return (
     <>
-      <p className={classes.stopwatchTime}>00:00:00</p>
+      <p className={classes.stopwatchTime}>{convertToHHMMSS(seconds)}</p>
     </>
   );
 };
--- src/pages/NewTime.js
+++ src/pages/NewTime.js
@@ -7,6 +7,7 @@ import { useState, useRef } from "react";
 
 const NewTime = () => {
   const [stopwatchIsRunning, setStopwatchIsRunning] = useState(false);
+  const [stopwatchStarted, setStopwatchStarted] = useState(false);
   const [shake, setShake] = useState(0);
 
   const inputRef = useRef();
@@ -15,6 +16,7 @@ const NewTime = () => {
     if (inputRef.current.value.trim().length === 0) {
       setShake(1);
     } else {
+      setStopwatchStarted(true);
       setStopwatchIsRunning(!stopwatchIsRunning);
     }
   };
@@ -25,14 +27,16 @@ const NewTime = () => {
         className={classes.pillContainer}
         onAnimationEnd={() => setShake(0)}
         shake={shake}
+        style={{ boxShadow: stopwatchIsRunning ? "0px 0px 15px #01315a" : "" }}
       >
         <input
           className={classes.inputField}
           type="text"
-          placeholder="What are you doing?"
+          placeholder="Start with a new task..."
+          disabled={stopwatchStarted}
           ref={inputRef}
         ></input>
-        <Stopwatch />
+        <Stopwatch stopwatchIsRunning={stopwatchIsRunning} />
         <button
           onClick={toggleStopwatchHandler}
           className={classes.stopwatchButton}
