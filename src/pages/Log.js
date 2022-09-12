6cbb24bf603fa28e738463acf63a54578a0a68bc
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-09-12 13:37:39 -0400

    Implement pagination

--- src/pages/Log.js
+++ src/pages/Log.js
@@ -3,8 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { useState, useContext } from "react";
 import { LogContext } from "../contexts/log-context";
 import Task from "../components/Task";
+import ReactPaginate from "react-paginate";
 
 const Log = () => {
+  const navigate = useNavigate();
   const { inEditMode, setInEditMode, convertToHHMMSS } = useContext(LogContext);
   const [log, setLog] = useState(
     localStorage.getItem("log") === null
@@ -12,7 +14,27 @@ const Log = () => {
       : JSON.parse(localStorage.getItem("log"))
   );
 
-  const navigate = useNavigate();
+  // React Paginate
+  const [pageNumber, setPageNumber] = useState(0);
+  const tasksPerPage = 5;
+  const pagesVisited = pageNumber * tasksPerPage;
+  const displayTasks = log
+    .slice(pagesVisited, pagesVisited + tasksPerPage)
+    .map((item) => {
+      return (
+        <Task
+          key={item.id}
+          task={item.task}
+          time={item.time}
+          setLog={setLog}
+          currentItemId={item.id}
+        />
+      );
+    });
+  const pageCount = Math.ceil(log.length / tasksPerPage);
+  const changePageHandler = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   const newHandler = () => {
     navigate("/");
@@ -46,18 +68,17 @@ const Log = () => {
         {log.length === 0 && (
           <p className={classes.emptyLog}>No logged tasks!</p>
         )}
-        {log.length > 0 &&
-          log.map((item) => {
-            return (
-              <Task
-                key={item.id}
-                task={item.task}
-                time={item.time}
-                setLog={setLog}
-                currentItemId={item.id}
-              />
-            );
-          })}
+        {log.length > 0 && displayTasks}
+        {log.length > 0 && (
+          <ReactPaginate
+            previousLabel={"<"}
+            nextLabel={">"}
+            pageCount={pageCount}
+            onPageChange={changePageHandler}
+            containerClassName={classes.paginationButtons}
+            activeClassName={classes.paginationActive}
+          />
+        )}
         <hr />
         <div className={classes.bottomSection}>
           {!inEditMode && (
